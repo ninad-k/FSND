97627efbfba4cc28e0a416a48d8c7ddad68f20bd
Ninad <ninadsanjaykulkarni@gmail.com>
2021-02-17 18:55:14 +0530

    Updated files as per review comments

--- projects/01_fyyur/starter_code/forms.py
+++ projects/01_fyyur/starter_code/forms.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField
 from wtforms.validators import DataRequired, URL
 
 
-class ShowForm(Form):
+class ShowForm(FlaskForm):
     artist_id = StringField(
         'artist_id'
     )
@@ -15,11 +15,11 @@ class ShowForm(Form):
     start_time = DateTimeField(
         'start_time',
         validators=[DataRequired()],
-        default= datetime.today()
+        default=datetime.today()
     )
 
-class VenueForm(Form):
 
+class VenueForm(FlaskForm):
     name = StringField(
         'name', validators=[DataRequired()]
     )
@@ -133,7 +133,8 @@ class VenueForm(Form):
         'seeking_description'
     )
 
-class ArtistForm(Form):
+
+class ArtistForm(FlaskForm):
     name = StringField(
         'name', validators=[DataRequired()]
     )
@@ -230,7 +231,7 @@ class ArtistForm(Form):
         'facebook_link', validators=[URL(require_tld=True, message='please enter valid URL link')]
     )
     website = StringField(
-        'website', validators=[URL()]
+        # 'website', validators=[URL()]
     )
     image_link = StringField(
         'image_link', validators=[URL()]
